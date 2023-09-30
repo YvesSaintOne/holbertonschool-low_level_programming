9e34a7b886e880e7d7029ec9f867e234ff1e82b3
YvesSaintOne <onterrionw@gmail.com>
2023-09-30 15:17:38 -0700

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * mul - function that multiplies two integers
- * @a: - parameter for function 
+ * @a: - parameter for function
  * @b: - parameter for function
  * Return: 0
  */
